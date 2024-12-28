9cf19c8fdd498a44276541e2b9e47e390a925fba
Sumit <owlisfragile@gmail.com>
2024-12-28 22:40:02 +0530

    hehe

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { PiLinkSimpleBold } from "react-icons/pi";
+
 interface CardProps {
   href: string;
   imageSrc: string;
@@ -10,9 +12,10 @@ interface CardProps {
 
 export default function Card({ href, imageSrc, altText, title }: CardProps) {
   return (
-    <div className="max-w-72 min-h-32 group/cards border hover:border-main_text hover:shadow-lg hover:transition-all hover:transform hover:scale-[1.02] rounded-lg">
+    <div className="relative max-w-72 min-h-32 group/cards border hover:border-main_text hover:shadow-lg hover:transition-all hover:transform hover:scale-[1.02] rounded-lg">
       <Link target="_blank" href={href}>
         <div className="">
+          <PiLinkSimpleBold className="absolute top-2 right-2 text-lg z-10 text-main_text hidden group-hover/cards:block group-hover/cards:animate-bounce" />
           <Image
             className="rounded-lg"
             src={imageSrc}
@@ -20,7 +23,9 @@ export default function Card({ href, imageSrc, altText, title }: CardProps) {
             width={500}
             height={500}
           />
-          <p className="text-center py-2 font-bold">{title}</p>
+          <p className="absolute z-10 bottom-0 right-0 left-0 rounded-ee-lg rounded-es-lg bg-gradient-to-t from-main_text/80 via-main_text/40 to-transparent backdrop-blur-md text-center py-2 font-bold dark:text-black">
+            {title}
+          </p>
         </div>
       </Link>
     </div>
