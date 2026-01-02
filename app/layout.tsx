8fcda2a90e2e0dde0b6d45285ce45b6e0e15028a
Sumit Kumar <97524217+sumit6702@users.noreply.github.com>
2026-01-03 04:56:23 +0530

    Remove unused Caveat font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
-import { Caveat } from "next/font/google";
 import Footer from "@/components/Footer";
 import { Providers } from "./providers";
 import { certia, main } from "./fonts";
