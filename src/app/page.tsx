1284060fc43721569bf82262a04e7de192b906e8
Sumit <your@email.com>
2026-08-22 22:16:16 +0530

    style: streamline homepage projects display and tighten top padding on work page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,9 +100,6 @@ export default function Home() {
           </RevealFx>
         </Column>
       </Column>
-      <RevealFx translateY="16" delay={0.6}>
-        <Projects range={[1, 1]} />
-      </RevealFx>
       {routes["/blog"] && (
         <Column fillWidth gap="24" marginBottom="l">
           <Row fillWidth paddingRight="64">
@@ -123,7 +120,7 @@ export default function Home() {
           </Row>
         </Column>
       )}
-      <Projects range={[2]} />
+      <Projects showFilters />
       <Mailchimp />
     </Column>
   );
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -14,7 +14,7 @@ export async function generateMetadata() {
 
 export default function Work() {
   return (
-    <Column maxWidth="m" paddingTop="24">
+    <Column maxWidth="m" paddingTop="12">
       <Schema
         as="webPage"
         baseURL={baseURL}
